3654477eaa936e6ca330c3ac538a8f9c572e994b
Deadly-Pigg <100409468+Deadly-Pigg@users.noreply.github.com>
2024-11-17 15:59:28 +0000

    task2

--- src/main/java/com/jpmc/midascore/T2_Listener.java
+++ src/main/java/com/jpmc/midascore/T2_Listener.java
@@ -1,12 +1,14 @@
 package com.jpmc.midascore;
 
+import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.listener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class T2_Listener  {
-    @KafkaListener(topics = "${general.kafka-topic}")
-    public void listen(String record)
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "temp")
+    public void listen(Transaction record)
     {
-
+        System.out.println(record);
     }
 }
